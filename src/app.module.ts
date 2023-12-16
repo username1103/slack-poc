2adc81b5ed0ecccd0846247cb4ce316357f14427
username1103 <mikim961103@gmail.com>
2023-12-16 23:51:18 +0900

    poc2

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,9 +13,11 @@ import {
 } from './slack/decorator/slack-method';
 import { EventSubscriptionStrategy } from './slack/method-strategy/event-subscription-strategy';
 import { CommandStrategy } from './slack/method-strategy/command-strategy';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [DiscoveryModule, SampleModule],
+  controllers: [AppController],
   providers: [EventSubscriptionStrategy, CommandStrategy],
 })
 export class AppModule implements OnModuleInit {
@@ -27,37 +29,37 @@ export class AppModule implements OnModuleInit {
   ) {}
 
   onModuleInit() {
-    const slackControllers = this.discoveryService
-      .getProviders()
-      .filter(
-        (wrapper) =>
-          wrapper.metatype &&
-          Reflect.hasMetadata(SLACK_CONTROLLER, wrapper.metatype),
-      );
-
-    slackControllers.forEach((wrapper) => {
-      const slackController = wrapper.instance;
-      const methods = this.metadataScanner.getAllMethodNames(slackController);
-
-      methods.forEach((method) => {
-        const metadataKeys = Reflect.getMetadataKeys(
-          slackController,
-          method,
-        ) as SlackMethod[];
-
-        metadataKeys.forEach((key) => {
-          switch (key) {
-            case EVENT_SUBSCRIPTION:
-              this.eventSubscriptionStrategy.route(slackController, method);
-              break;
-            case COMMAND:
-              this.commandStrategy.route(slackController, method);
-              break;
-            default:
-              break;
-          }
-        });
-      });
-    });
+    // const slackControllers = this.discoveryService
+    //   .getProviders()
+    //   .filter(
+    //     (wrapper) =>
+    //       wrapper.metatype &&
+    //       Reflect.hasMetadata(SLACK_CONTROLLER, wrapper.metatype),
+    //   );
+    //
+    // slackControllers.forEach((wrapper) => {
+    //   const slackController = wrapper.instance;
+    //   const methods = this.metadataScanner.getAllMethodNames(slackController);
+    //
+    //   methods.forEach((method) => {
+    //     const metadataKeys = Reflect.getMetadataKeys(
+    //       slackController,
+    //       method,
+    //     ) as SlackMethod[];
+    //
+    //     metadataKeys.forEach((key) => {
+    //       switch (key) {
+    //         case EVENT_SUBSCRIPTION:
+    //           this.eventSubscriptionStrategy.route(slackController, method);
+    //           break;
+    //         case COMMAND:
+    //           this.commandStrategy.route(slackController, method);
+    //           break;
+    //         default:
+    //           break;
+    //       }
+    //     });
+    //   });
+    // });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SlackAdapter } from './slack/slack-adapter';
+import { App, ExpressReceiver, LogLevel } from '@slack/bolt';
 
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
-    new SlackAdapter({
-      signingSecret: process.env.SIGNING_SECRET,
-      clientSecret: process.env.CLIENT_SECRET,
-      clientId: process.env.CLIENT_ID,
-      token: process.env.BOT_OAUTH_TOKEN,
-    }),
+    // new SlackAdapter({
+    //   signingSecret: process.env.SIGNING_SECRET,
+    //   clientSecret: process.env.CLIENT_SECRET,
+    //   clientId: process.env.CLIENT_ID,
+    //   token: process.env.BOT_OAUTH_TOKEN,
+    // }),
   );
+  const express = app.getHttpAdapter().getInstance();
+  const receiver = new ExpressReceiver({
+    signingSecret: '833ceac41957bd1c41352023140facf8',
+    clientId: '453217301505.6352523444162',
+    clientSecret: '4bce9ce16983fca21e3efcedd6f79464',
+    app: express,
+  });
+
+  const slackApp = new App({
+    token: 'xoxb-453217301505-6349626811829-9kqqyvg9ND4DHJzvM4ZeFS6W',
+    receiver,
+    logLevel: LogLevel.DEBUG,
+  });
+
+  slackApp.command('/커맨드', async ({ ack }) => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await ack();
+  });
+
   await app.listen(3000);
 }
 bootstrap();
